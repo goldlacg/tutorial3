de1022a5a744f0f54d895efb323e825fca9f1390
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-11 01:39:45 +0000

    Nov 10, 2022, 8:39 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -51,6 +51,15 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
