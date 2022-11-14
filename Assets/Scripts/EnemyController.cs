f0f33a1b91970384d4edb5199820b5aa652e7a2f
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-14 20:00:51 +0000

    Nov 14, 2022, 3:00 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,15 +7,19 @@ public class EnemyController : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigidbody2D;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + Time.deltaTime * speed;
+
+        rigidbody2D.MovePosition(position);
     }
 }
