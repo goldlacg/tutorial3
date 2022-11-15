9b5292a1988093d15ef21b638ca2d174af9357d4
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-15 17:38:48 +0000

    Nov 15, 2022, 12:38 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -22,6 +22,8 @@ public class RubyController : MonoBehaviour
     Animator animator;
     Vector2 lookDirection = new Vector2(1, 0);
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
