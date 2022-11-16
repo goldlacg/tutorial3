6ba4fdb5d9e5c9b2fcc12a02ca75c564a9033390
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-16 03:12:01 +0000

    Nov 15, 2022, 10:12 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -93,7 +93,7 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
