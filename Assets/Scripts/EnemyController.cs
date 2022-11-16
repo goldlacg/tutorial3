bb522cd222cbcdfaac4f93d0549ac3496d98f9f3
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-16 01:37:35 +0000

    Nov 15, 2022, 8:37 PM

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -82,6 +82,7 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2D.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 
 }
