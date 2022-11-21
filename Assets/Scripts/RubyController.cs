35b21b4c2cf35f2d86623c21e78f25db6630e4b8
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-21 05:39:49 +0000

    Nov 21, 2022, 12:39 AM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -110,7 +110,7 @@ public class RubyController : MonoBehaviour
 
         if (amount > 0)
         {
-            ParticleSystem heal = Instantiate(healthEffect, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+            
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
