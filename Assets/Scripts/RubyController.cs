99ba4dad6baf698e9eb2e582b1083b3d4e249088
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-22 17:45:03 +0000

    Nov 22, 2022, 12:45 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -117,6 +117,11 @@ public class RubyController : MonoBehaviour
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
+    public void PlayEffect(ParticleSystem healthEffect)
+    {
+        particle.Play(healthEffect);
+    }
+
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
