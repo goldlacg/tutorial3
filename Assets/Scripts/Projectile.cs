2573116627ff6233ed8168b790feee56b7afb5e3
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-22 22:04:14 +0000

    Nov 22, 2022, 5:04 PM

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -33,6 +33,7 @@ public class Projectile : MonoBehaviour
         {
             e.Fix();
             RubyController.robotsFixed += 1;
+            Debug 
         }
         Destroy(gameObject);
 
