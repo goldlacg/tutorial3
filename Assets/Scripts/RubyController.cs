1e07c744b46d6f0a9f1e82882c283d314055f4a6
goldlacg <111778196+goldlacg@users.noreply.github.com>
2022-11-23 00:08:12 +0000

    Nov 22, 2022, 7:08 PM

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -37,7 +37,8 @@ public class RubyController : MonoBehaviour
     public ParticleSystem healthEffect;
     public GameObject collectibleHealth;
 
-    AudioSource audioSource;
+    public AudioSource audioSource;
+    public AudioClip bkgSound;
     public AudioClip throwSound;
     public AudioClip hitSound;
     public AudioClip winSound;
